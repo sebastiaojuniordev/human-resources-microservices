003448d9b348cff7c45315d56abbf5a3853e2618
Sebastião Junior <77406058+sebastiaojuniordev@users.noreply.github.com>
2021-04-07 20:11:40 -0300

    Eureka server

---
+++ hr-eureka-server/src/test/java/br/com/sebastiaojunior/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sebastiaojunior.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
