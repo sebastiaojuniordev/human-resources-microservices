3da41a66c22a209dabe8223e043f9eb3c7f159b8
Sebastião Junior <77406058+sebastiaojuniordev@users.noreply.github.com>
2021-04-07 21:21:52 -0300

    Project hr-api-gateway zuul created

---
+++ hr-api-gateway-zuul/src/test/java/br/com/sebastiaojunior/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sebastiaojunior.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
