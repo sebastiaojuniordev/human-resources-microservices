868d37e520ca62edd8edbf13c7c0c9546356d2d5
Sebastião Junior <77406058+sebastiaojuniordev@users.noreply.github.com>
2021-04-13 20:36:08 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/br/com/sebastiaojunior/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sebastiaojunior.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
